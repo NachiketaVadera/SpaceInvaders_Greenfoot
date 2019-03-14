f78444d45e602937b84b8926e22e8f6ff66c50d7
Nachiketa Vadera <personal.nachiketa@gmail.com>
2019-03-15 00:06:33 +0530

    GameOver actor class screen

---
+++ GameOver.java
@@ -0,0 +1,7 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+public class GameOver extends Actor {
+    public GameOver() {
+        setImage("GameOver.gif");
+    }
+}
